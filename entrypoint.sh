89d7356abf6ca43a12d6f86d0151c81b415f6a0f
B.K.Jayasundera <57324571+bkjaya2020@users.noreply.github.com>
2020-01-20 15:49:22 +0530

    As at 20-01-2020

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
-
 service mysql start
- mysql
-CREATE USER 'admin'@'localhost' IDENTIFIED WITH mysql_native_password BY '';
-GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES ;
-quit
-/etc/init.d/mysql restart
-mysql
-CREATE USER 'zmuser'@localhost IDENTIFIED WITH mysql_native_password BY 'zmpass';
-GRANT ALL PRIVILEGES ON zm.* TO 'zmuser'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES ;
-quit
-mysqladmin -uroot -p reload
 systemctl enable zoneminder
-/etc/init.d/zoneminder start
-/etc/init.d/apache2 restart
-
+/usr/bin/zmpkg.pl start
+/etc/init.d/apache2 start
 tail -F n0 /dev/null
