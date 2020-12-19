105cda6d3370a7db9c8d148c4505b95303cb5929
B.K.Jayasundera <57324571+bkjaya2020@users.noreply.github.com>
2020-12-19 16:49:06 +0530

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -2,6 +2,5 @@ mysql -uroot -p < /usr/share/zoneminder/db/zm_create.sql
 mysql -e "CREATE USER 'zmuser'@localhost IDENTIFIED BY 'zmpass';"
 mysql -e "GRANT ALL PRIVILEGES ON zm.* TO 'zmuser'@'localhost' WITH GRANT OPTION;"
 mysql -e "FLUSH PRIVILEGES ;"
-mysqladmin -uroot -p reload
 dpkg-reconfigure tzdata
 exit
