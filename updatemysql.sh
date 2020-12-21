1a3e77f9016ca8bb01c6a6b344c2008a05a142a0
B.K.Jayasundera <57324571+bkjaya2020@users.noreply.github.com>
2020-12-21 22:21:48 +0530

    Update updatemysql.sh

--- updatemysql.sh
+++ updatemysql.sh
@@ -1,8 +1,7 @@
+#!/bin/bash
 mysql -e "drop database zm;"
-mysql -uroot -p < /usr/share/zoneminder/db/zm_create.sql
+mysql -uroot --password=""< /usr/share/zoneminder/db/zm_create.sql 2>/dev/null
 mysql -e "ALTER USER 'zmuser'@localhost IDENTIFIED BY 'zmpass';"
 mysql -e "GRANT ALL PRIVILEGES ON zm.* TO 'zmuser'@'localhost' WITH GRANT OPTION;"
 mysql -e "FLUSH PRIVILEGES ;"
-mysqladmin -uroot -p reload
-dpkg-reconfigure tzdata
 exit
