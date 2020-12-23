e64dd47bb5b3a82456e366843f59244e1894e1cf
B.K.Jayasundera <57324571+bkjaya2020@users.noreply.github.com>
2020-12-23 11:41:04 +0530

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -3,4 +3,5 @@ mysql -uroot --password=""< /usr/share/zoneminder/db/zm_create.sql 2>/dev/null
 mysql -e "CREATE USER 'zmuser'@localhost IDENTIFIED BY 'zmpass';"
 mysql -e "GRANT ALL PRIVILEGES ON zm.* TO 'zmuser'@'localhost' WITH GRANT OPTION;"
 mysql -e "FLUSH PRIVILEGES ;"
+mysqladmin -uroot --password="" reload 2>/dev/null
 exit
