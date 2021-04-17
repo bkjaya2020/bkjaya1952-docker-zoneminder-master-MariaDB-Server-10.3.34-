819c891620f40453d25e44ba1a1c58af300c897a
B.K.Jayasundera <57324571+bkjaya2020@users.noreply.github.com>
2021-04-17 20:44:18 +0530

    Update zm.sh

--- zm.sh
+++ zm.sh
@@ -1,9 +1,2 @@
 add-apt-repository ppa:iconnor/zoneminder-1.34 
 apt -y install zoneminder 
-/etc/init.d/mysql start
-cp zm_create.sql /usr/share/zoneminder/db/zm_create.sql
-mysql -uroot --password=""< /usr/share/zoneminder/db/zm_create.sql 2>/dev/null
-mysql -e "ALTER USER 'zmuser'@localhost IDENTIFIED BY 'zmpass';"
-mysql -e "GRANT ALL PRIVILEGES ON zm.* TO 'zmuser'@'localhost' WITH GRANT OPTION;"
-mysql -e "FLUSH PRIVILEGES ;"
-mysqladmin -uroot --password="" reload 2>/dev/null
