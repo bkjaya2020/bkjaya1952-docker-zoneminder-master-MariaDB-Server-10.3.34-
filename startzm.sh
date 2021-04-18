53e0e9567849550648ca5c7eca81bb8d9a091c9f
B.K.Jayasundera <57324571+bkjaya2020@users.noreply.github.com>
2021-04-18 11:43:49 +0530

    Update startzm.sh

--- startzm.sh
+++ startzm.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 /etc/init.d/mysql start
+mysql -e "drop database zm;"
+mysql -uroot --password=""< /usr/share/zoneminder/db/zm_create.sql 2>/dev/null
+mysql -e "ALTER USER 'zmuser'@localhost IDENTIFIED BY 'zmpass';"
+mysql -e "GRANT ALL PRIVILEGES ON zm.* TO 'zmuser'@'localhost' WITH GRANT OPTION;"
+mysql -e "FLUSH PRIVILEGES ;"
 /etc/init.d/apache2 start
 chmod 777 /var/run/zm
 /usr/bin/zmpkg.pl start
