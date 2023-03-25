326bb5d64b3377d6ecc44ebcb992f94a593cb5b7
B.K.Jayasundera <57324571+bkjaya2020@users.noreply.github.com>
2023-03-25 12:28:57 +0530

    Update zm.sh

--- zm.sh
+++ zm.sh
@@ -2,10 +2,11 @@ add-apt-repository ppa:iconnor/zoneminder-master
 apt -y install mariadb-server
 apt -y install zoneminder 
 /etc/init.d/mysql start
+zmupdate.pl
 mysql -uroot --password=""< /usr/share/zoneminder/db/zm_create.sql 2>/dev/null
 mysql -e "CREATE USER 'zmuser'@localhost IDENTIFIED BY 'zmpass';"
 mysql -e "GRANT ALL PRIVILEGES ON zm.* TO 'zmuser'@'localhost' WITH GRANT OPTION;"
 mysql -e "FLUSH PRIVILEGES ;"
 mysqladmin -uroot --password="" reload 2>/dev/null
-mysql -u zmuser -p zm < /usr/share/zoneminder/db/zm_update-1.37.27.sql
+
 
