5da7cb88f0922d63d08916d32b7db89652cbb1c1
B.K.Jayasundera <57324571+bkjaya2020@users.noreply.github.com>
2023-03-26 17:12:46 +0530

    Update zm.sh

--- zm.sh
+++ zm.sh
@@ -2,7 +2,6 @@ add-apt-repository ppa:iconnor/zoneminder-master
 apt -y install mariyadb-server
 apt -y install zoneminder 
 /etc/init.d/mysql start
-zmupdate.pl
 mysql -uroot --password=""< /usr/share/zoneminder/db/zm_create.sql 2>/dev/null
 mysql -e "CREATE USER 'zmuser'@localhost IDENTIFIED BY 'zmpass';"
 mysql -e "GRANT ALL PRIVILEGES ON zm.* TO 'zmuser'@'localhost' WITH GRANT OPTION;"
